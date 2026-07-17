88d5cf009263af5c7e43f95737c08dc521f7478f
Moazzem-Rafi <mhr8june@gmail.com>
2026-07-17 21:09:26 +0600

    rewrite

--- backend/controllers/deleteController.js
+++ backend/controllers/deleteController.js
@@ -5,61 +5,8 @@ const deleteTodo = async (req,res)=>{
     const deleteData = await Todo.findByIdAndDelete(id)
     res.send({
         status : true,
-<<<<<<< HEAD
-        massage : "Todo Deleted successfully!"
-=======
-        massage : "Todo Deleted "
->>>>>>> 1229f741919ec012c14d783866a80643d38b38b5
+        massage : "Todo Deleted succcessfully!"
     })
 }
 
 module.exports = deleteTodo
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 1229f741919ec012c14d783866a80643d38b38b5
-// const Todo = require ("../models/todoModel")
-
-// const deleteTodo = async (req,res)=>{
-//   const {id} = req.params
-
-//   const deleteData = await Todo.findByIdAndDelete(id)
-
-//   res.send ({
-//     success : true,
-//     massage : "Todo Deleted"
-//   })
-// }
-
-
-// module.exports = deleteTodo
